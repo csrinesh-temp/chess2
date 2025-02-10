02364fe20175b52dc1ba0ad1d860e26b0df327f1
csrinesh <csrinesh@gmail.com>
2025-02-10 12:06:53 +0530

    add icons

--- mains.py
+++ mains.py
@@ -1,5 +1,7 @@
+import os
 import tkinter as tk
 from tkinter import Canvas
+from tkinter import PhotoImage
 import chess
 
 class ChessApp:
@@ -11,8 +13,18 @@ class ChessApp:
         self.canvas.pack()
         self.sq_size = 50
         self.selected_square = None
+        self.piece_images = {}
+        self.load_images()
         self.create_board()
         
+    def load_images(self):
+        pieces = ['K', 'Q', 'R', 'B', 'N', 'P', 'k', 'q', 'r', 'b', 'n', 'p']
+        map = {'K': 'wk', 'Q': 'wq', 'R': 'wr', 'B': 'wb', 'N': 'wkn', 'P': 'wp',
+               'k': 'bk', 'q': 'bq', 'r': 'br', 'b': 'bb', 'n': 'bkn', 'p': 'bp'}
+        for piece in pieces:
+            piece_name = map.get(piece, None)
+            self.piece_images[piece] = PhotoImage(file=os.path.join('images', f'{piece_name}.png'))
+
     def create_board(self):
         for row in range(8):
             for col in range(8):
@@ -29,7 +41,8 @@ class ChessApp:
         for square, piece in piece_symbols.items():
             x = chess.square_file(square) * self.sq_size
             y = (7 - chess.square_rank(square)) * self.sq_size
-            self.canvas.create_text(x + self.sq_size // 2, y + self.sq_size // 2, text=piece.symbol(), tags="piece", font=("Arial", 24))
+            image = self.piece_images[piece.symbol()]
+            self.canvas.create_image(x + self.sq_size // 2, y + self.sq_size // 2, image=image, tags="piece")
 
     def on_click(self, event):
         col = event.x // self.sq_size
